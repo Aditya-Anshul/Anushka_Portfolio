dd3c73070a2e686d02dcafc8ea4686b23e2b41bf
Aditya Anshul <68283200+Aditya-Anshul@users.noreply.github.com>
2022-12-21 21:16:02 +0530

    Update socialsData.js

--- profile-fe/src/data/socialsData.js
+++ profile-fe/src/data/socialsData.js
@@ -1,5 +1,6 @@
 export const socialsData = {
     linkedIn: 'https://www.linkedin.com/in/anushka-srivastava-b39904203/',
     instagram: 'https://www.instagram.com/anushka_srivastava04',
-    youtube: 'https://youtube.com/'
+    youtube: 'https://youtube.com/',
+    behance: 'https://www.behance.net/anushka_04',
 }
